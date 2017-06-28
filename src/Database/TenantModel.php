764e61d4c4ac714466b2c608eba79c47c67213a3
Bruno Goncalves <bugotech@gmail.com>
2017-06-28 00:27:02 -0300

    ajustes namespace

--- src/Database/TenantModel.php
+++ src/Database/TenantModel.php
@@ -22,12 +22,12 @@ trait TenantModel
             }
 
             // Verificar se usuário esta logado
-            if (auth()::check() != true) {
+            if (auth()->check() != true) {
                 error('Usuário não esta logado');
             }
 
             // Setar inquilino
-            $model->inquilino_id = auth()::user()->inquilino_id;
+            $model->inquilino_id = auth()->user()->inquilino_id;
         });
     }
 }
