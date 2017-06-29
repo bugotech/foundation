4a16ac2a8f2e848380b1c63646725def7d5ff947
Bruno Goncalves <bugotech@gmail.com>
2017-06-29 10:57:43 -0300

    defaultValues

---
+++ src/Database/DefaultValuesModel.php
@@ -0,0 +1,50 @@
+<?php namespace Bugotech\Foundation\Database;
+
+trait DefaultValuesModel
+{
+    /**
+     * Lista de attributos com valor padrão.
+     * @var array
+     */
+    public $defaultValues = [];
+
+    /**
+     * Registrar eventos.
+     */
+    public static function bootDefaultValuesModel()
+    {
+        // Registrar evento para carregar valores padrões.
+        self::loaded(function ($model) {
+            $model->loadDefaultValues();
+        });
+    }
+
+    /**
+     * Carregar valores padrões.
+     */
+    protected function loadDefaultValues()
+    {
+        // Aplicar valores
+        foreach ($this->defaultValues as $name => $value) {
+            $value = $this->resolveValue($value);
+            $this->setAttribute($name, $value);
+        }
+    }
+
+    /**
+     * Tratar valores e funções.
+     * @param $value
+     * @return string|mixed
+     */
+    protected function resolveValue($value)
+    {
+        $code = '$value = %s;';
+        $value = is_string($value) ? sprintf('"%s"', $value) : $value;
+        $code = sprintf($code, $value);
+
+        // Executar codigo
+        eval($code);
+
+        return $value;
+    }
+}
--- src/Database/Model.php
+++ src/Database/Model.php
@@ -6,6 +6,46 @@ class Model extends Eloquent
 {
     use CastModel;
     use ValidatorModel;
+    use DefaultValuesModel;
 
     public $timestamps = false;
+
+    /**
+     * Create a new Eloquent model instance.
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->fireModelEvent('loading', false);
+
+        parent::__construct($attributes);
+
+        $this->fireModelEvent('loaded', false);
+    }
+
+    /**
+     * Register a loading model event with the dispatcher.
+     *
+     * @param  \Closure|string  $callback
+     * @param  int  $priority
+     * @return void
+     */
+    public static function loading($callback, $priority = 0)
+    {
+        static::registerModelEvent('loading', $callback, $priority);
+    }
+
+    /**
+     * Register a loaded model event with the dispatcher.
+     *
+     * @param  \Closure|string  $callback
+     * @param  int  $priority
+     * @return void
+     */
+    public static function loaded($callback, $priority = 0)
+    {
+        static::registerModelEvent('loaded', $callback, $priority);
+    }
 }
